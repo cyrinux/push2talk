4e019c913d532133910ed4bb36942582d2d11093
Maxim Baz <git@maximbaz.com>
2023-10-31 19:50:54 +0100

    Remove expects

--- src/pulseaudio/mod.rs
+++ src/pulseaudio/mod.rs
@@ -15,15 +15,15 @@ impl Controller {
     }
 
     pub fn run(&self, rx: Receiver<(bool, Option<String>)>) -> Result<(), Box<dyn Error>> {
-        let mut mainloop = Mainloop::new().expect("Failed to create mainloop");
+        let mut mainloop = Mainloop::new().ok_or("Failed to create mainloop")?;
 
         let mut context =
-            Context::new(&mainloop, "Push2talk").expect("Failed to create new context");
+            Context::new(&mainloop, "Push2talk").ok_or("Failed to create new context")?;
 
         context.connect(None, FlagSet::NOFLAGS, None)?;
 
         // Wait for context to be ready
-        mainloop.start().expect("Start mute loop");
+        mainloop.start()?;
         loop {
             thread::sleep(Duration::from_millis(250));
             if context.get_state() == libpulse_binding::context::State::Ready {
